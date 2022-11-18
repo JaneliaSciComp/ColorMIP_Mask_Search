0d579f65d864584de984a3168d31f367a9611c17
HideoOtsuna <otsuna@muc.biglobe.ne.jp>
2022-11-18 14:01:55 -0500

    Fiji plugin

--- Color_MIP_Mask_Search.java
+++ Color_MIP_Mask_Search.java
@@ -4,16 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-import java.util.HashSet;
-=======
->>>>>>> parent of f1d5276 (horizontal image)
-=======
->>>>>>> parent of f1d5276 (horizontal image)
-=======
->>>>>>> parent of f1d5276 (horizontal image)
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
@@ -315,19 +305,6 @@ public class Color_MIP_Mask_Search implements PlugInFilter
 		labelmethod=1;
 		
 		GenericDialog gd = new GenericDialog("ColorMIP_3D_Mask search");
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-		gd.setInsets(0, 340, 0);
-		gd.addCheckbox("Horizontal ", horizontal); //Horizontal
-		gd.setInsets(5, 0, 0);
-		
-=======
->>>>>>> parent of f1d5276 (horizontal image)
-=======
->>>>>>> parent of f1d5276 (horizontal image)
-=======
->>>>>>> parent of f1d5276 (horizontal image)
 		gd.addChoice("Mask", titles, titles[Mask]); //Mask
 		gd.addSlider("1.Threshold for mask", 0, 255, Thresm);
 		gd.setInsets(0, 340, 0);
@@ -423,9 +400,7 @@ public class Color_MIP_Mask_Search implements PlugInFilter
 		if(GCON==true)
 		System.gc();
 		
-		int tz=0;
-		
-		if(logon==true && tz==1){
+		if(logon==true){
 			GenericDialog gd2 = new GenericDialog("log option");
 			gd2.addCheckbox("ShowNaN",logNan);
 			
@@ -511,41 +486,9 @@ public class Color_MIP_Mask_Search implements PlugInFilter
 			return;
 		}
 		
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-		if (horizontal)
-		{
-			for(int ix=950; ix<width; ix++){// deleting color scale from mask
-				for(int iy=335-85; iy<335; iy++){
-					ip1.set(ix,iy,-16777216);
-				}
-				for(int iy=670-85; iy<670; iy++){
-					ip1.set(ix,iy,-16777216);
-				}
-			}
-		}
-		else
-		{
-			for(int ix=950; ix<width; ix++){// deleting color scale from mask
-				for(int iy=0; iy<85; iy++){
-					ip1.set(ix,iy,-16777216);
-				}
-=======
 		for(int ix=950; ix<width; ix++){// deleting color scale from mask
 			for(int iy=0; iy<85; iy++){
 				ip1.set(ix,iy,-16777216);
->>>>>>> parent of f1d5276 (horizontal image)
-=======
-		for(int ix=950; ix<width; ix++){// deleting color scale from mask
-			for(int iy=0; iy<85; iy++){
-				ip1.set(ix,iy,-16777216);
->>>>>>> parent of f1d5276 (horizontal image)
-=======
-		for(int ix=950; ix<width; ix++){// deleting color scale from mask
-			for(int iy=0; iy<85; iy++){
-				ip1.set(ix,iy,-16777216);
->>>>>>> parent of f1d5276 (horizontal image)
 			}
 		}
 		
@@ -609,7 +552,7 @@ public class Color_MIP_Mask_Search implements PlugInFilter
 				}
 			}
 			
-			IJ.log("compression; "+fileformat);
+			IJ.log("compressio; "+fileformat);
 		}
 		
 		ArrayList<String> srlabels = new ArrayList<String>();
@@ -1073,7 +1016,6 @@ public class Color_MIP_Mask_Search implements PlugInFilter
 		if(posislice>0){// if result is exist
 			int posislice2=posislice;
 			
-			//		if(logon==true)
 			ResultsTable rt = new ResultsTable();
 			
 			if(posislice==1)
@@ -1083,15 +1025,13 @@ public class Color_MIP_Mask_Search implements PlugInFilter
 			for(int CreateLineArray=0; CreateLineArray<posislice; CreateLineArray++){
 				linenameTmpo = srlabels.get(CreateLineArray);
 				
-				
-				
+
+
 				int LineBeginIndex=(linenameTmpo.indexOf("MB"));
 				if(LineBeginIndex==-1)
 				LineBeginIndex=(linenameTmpo.indexOf("GMR"));
 				if(LineBeginIndex==-1)
 				LineBeginIndex=(linenameTmpo.indexOf("VT"));
-				if(LineBeginIndex==-1)
-				LineBeginIndex=(linenameTmpo.indexOf("JRC_"));
 				if(LineBeginIndex==-1)
 				LineBeginIndex=(linenameTmpo.indexOf("_TR_"));
 				if(LineBeginIndex==-1)
@@ -1105,32 +1045,39 @@ public class Color_MIP_Mask_Search implements PlugInFilter
 				if(LineBeginIndex==-1)
 				LineBeginIndex=(linenameTmpo.indexOf("BJD"));
 				if(LineBeginIndex==-1)
-				LineBeginIndex=(linenameTmpo.indexOf("SS"));
-				if(LineBeginIndex==-1)
 				LineBeginIndex=(linenameTmpo.indexOf("UAH"));
 				if(LineBeginIndex==-1)
 				LineBeginIndex=(linenameTmpo.indexOf("OL"));
+				if(LineBeginIndex==-1)
+				LineBeginIndex=(linenameTmpo.indexOf("JRC_"));
+				else if(LineBeginIndex==-1)
+				LineBeginIndex=(linenameTmpo.indexOf("SS"));
 				
 				int DotPosi=(linenameTmpo.indexOf("."));
+				int lengthST = linenameTmpo.length();
 				
-				if(LineBeginIndex==-1)
-<<<<<<< HEAD
-				LineBeginIndex=(linenameTmpo.indexOf("_"));
-				//dupline=0;
-=======
-				dupline=0;
->>>>>>> parent of b45e1ea (Fiji plugin)
+				if(LineBeginIndex>(lengthST/2))
+				LineBeginIndex=-1;
+				
+				if(LineBeginIndex==-1){
+					int underindex = (linenameTmpo.indexOf("_"));
+					
+					if(underindex>(lengthST/3))
+					dupline=0;
+					else
+					LineBeginIndex=(linenameTmpo.indexOf("_"));
+					
+				}
 				
 				
-				
+	
 				if(dupline>0){
 					//	IJ.log("JRCPosi; "+JRCPosi);
 					
 					int hyphen=(linenameTmpo.indexOf("-", 0 ));
 					
 					if(LineBeginIndex!=-1){
-<<<<<<< HEAD
-						
+		
 						if(hyphen==-1){
 							int UnderS1=(linenameTmpo.indexOf("_", LineBeginIndex+1));
 							int UnderS2=(linenameTmpo.indexOf("_", UnderS1+1 ));// end of line number
@@ -1139,18 +1086,7 @@ public class Color_MIP_Mask_Search implements PlugInFilter
 							else 
 							LineNo=linenameTmpo.substring(0, DotPosi);
 						}else
-=======
-						int UnderS1=(linenameTmpo.indexOf("_", LineBeginIndex+1));
-						int UnderS2=(linenameTmpo.indexOf("_", UnderS1+1 ));// end of line number
-						
-						if(hyphen==-1)
-						LineNo=linenameTmpo.substring(LineBeginIndex, UnderS2);// GMR_01A02
-						else
->>>>>>> parent of b45e1ea (Fiji plugin)
 						LineNo=linenameTmpo.substring(LineBeginIndex, hyphen);// GMR_01A02
-						
-					//	IJ.log("LineNo; "+LineNo);
-						
 					}else{
 						if(hyphen!=-1)
 						LineNo=linenameTmpo.substring(0, hyphen);
@@ -1160,11 +1096,7 @@ public class Color_MIP_Mask_Search implements PlugInFilter
 				}else//	if(dupline>0){
 				LineNo=linenameTmpo.substring(0, DotPosi);
 				
-<<<<<<< HEAD
-				//	IJ.log("linenameTmpo; "+linenameTmpo+"dupline; "+dupline+"  LineBeginIndex; "+LineBeginIndex+"  LineNo; "+LineNo);
-=======
-			//	IJ.log("dupline; "+dupline+"  LineBeginIndex; "+LineBeginIndex+"  LineNo; "+LineNo);
->>>>>>> parent of b45e1ea (Fiji plugin)
+			//	IJ.log("linenameTmpo; "+linenameTmpo+"dupline; "+dupline+"  LineBeginIndex; "+LineBeginIndex+"  LineNo; "+LineNo);
 				String posipersent2ST;
 				if(labelmethod==0 || labelmethod==1){// on top score
 					int UnderS0=(linenameTmpo.indexOf("_"));
@@ -1558,8 +1490,8 @@ public class Color_MIP_Mask_Search implements PlugInFilter
 															//	posislice2=posislice2-1;
 															//	posislice2=dcStack.size();
 															
-															//			if(logon==true)
-															//			IJ.log(String.valueOf(FinalAdded)+"  Added Slice; "+FullLinenamePosi2);
+													//		if(DUPlogon==true)
+													//		IJ.log("   "+FinalAdded+"  Added Slice; "+FullLinenamePosi2);
 															
 															FinalAdded=FinalAdded+1;
 															LineNameArray[PosiSliceScan+AddedSlice]="Deleted";
@@ -1675,8 +1607,6 @@ public class Color_MIP_Mask_Search implements PlugInFilter
 			
 			int[] m_mask2;
 			m_mask2 = get_mskpos_array(ip1, Thresm);
-			
-			
 			
 			//	VirtualStack OrigiStackfinal = new VirtualStack (width,height);
 			int slnum =0;
@@ -1918,8 +1848,6 @@ public class Color_MIP_Mask_Search implements PlugInFilter
 			if(logon==true)
 			rt.show("Results");
 		}//if(posislice>0){
-		
-		
 		
 		end = System.currentTimeMillis();
 		//		int gap=(int)(end-start)/1000;
@@ -2342,17 +2270,6 @@ public class Color_MIP_Mask_Search implements PlugInFilter
 			m_mirror = mirror_mask;
 			m_mirrorneg = mirror_negmask;
 			m_xyshift = xyshift;
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-			
-			m_horizontal = horizontal;
-=======
->>>>>>> parent of f1d5276 (horizontal image)
-=======
->>>>>>> parent of f1d5276 (horizontal image)
-=======
->>>>>>> parent of f1d5276 (horizontal image)
 			
 			//shifting
 			m_tarmasklist = generate_shifted_masks(m_mask, m_xyshift, m_width, m_height);
@@ -2548,19 +2465,6 @@ public class Color_MIP_Mask_Search implements PlugInFilter
 			
 			int masksize = srcmaskposi.length <= tarmaskposi.length ? srcmaskposi.length : tarmaskposi.length;
 			int posi = 0;
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-			
-			int center = m_width * m_height / 2;
-			
-			HashSet<Integer> scset = new HashSet<Integer>();
-=======
->>>>>>> parent of f1d5276 (horizontal image)
-=======
->>>>>>> parent of f1d5276 (horizontal image)
-=======
->>>>>>> parent of f1d5276 (horizontal image)
 			for(int masksig=0; masksig<masksize; masksig++){
 				
 				if (srcmaskposi[masksig] == -1 || tarmaskposi[masksig] == -1) continue;
@@ -2585,336 +2489,16 @@ public class Color_MIP_Mask_Search implements PlugInFilter
 							coloc_out[p+1] = tar[p+1];
 							coloc_out[p+2] = tar[p+2];
 						}
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-						
-						if (m_horizontal)
-						{
-							if (tarmaskposi[masksig] >= center)
-							{
-								if (!scset.contains(tarmaskposi[masksig]-center))
-								posi++;
-							}
-							else
-							scset.add(tarmaskposi[masksig]);
-						}
-						else
-=======
->>>>>>> parent of f1d5276 (horizontal image)
-=======
->>>>>>> parent of f1d5276 (horizontal image)
-=======
->>>>>>> parent of f1d5276 (horizontal image)
 						posi++;
 					}
 					
 				}
 			}
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-			
-			if (m_horizontal)
-			posi += scset.size();
-=======
->>>>>>> parent of f1d5276 (horizontal image)
-=======
->>>>>>> parent of f1d5276 (horizontal image)
-=======
->>>>>>> parent of f1d5276 (horizontal image)
 			
 			return posi;
 			
 		}
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-		
-		public int calc_score(ImageProcessor src, int[] srcmaskposi, ImageProcessor tar, int[] tarmaskposi, int th, double pixfludub, ImageProcessor coloc_out) {
-			
-			int masksize = srcmaskposi.length <= tarmaskposi.length ? srcmaskposi.length : tarmaskposi.length;
-			int posi = 0;
-			
-			int center = m_width * m_height / 2;
-			
-			HashSet<Integer> scset = new HashSet<Integer>();
-			for(int masksig=0; masksig<masksize; masksig++){
-				
-				if (srcmaskposi[masksig] == -1 || tarmaskposi[masksig] == -1) continue;
-				
-				int pix1= src.get(srcmaskposi[masksig]);
-				int red1 = (pix1>>>16) & 0xff;
-				int green1 = (pix1>>>8) & 0xff;
-				int blue1 = pix1 & 0xff;
-				
-				int pix2= tar.get(tarmaskposi[masksig]);
-				int red2 = (pix2>>>16) & 0xff;
-				int green2 = (pix2>>>8) & 0xff;
-				int blue2 = pix2 & 0xff;
-				
-				if(red2>th || green2>th || blue2>th){
-					
-					double pxGap = calc_score_px(red1, green1, blue1, red2, green2, blue2); 
-					
-					if(pxGap<=pixfludub){
-						if(coloc_out!=null)
-						coloc_out.set(tarmaskposi[masksig], pix2);
-						
-						if (m_horizontal)
-						{
-							if (tarmaskposi[masksig] >= center)
-							{
-								if (!scset.contains(tarmaskposi[masksig]-center))
-								posi++;
-							}
-							else
-							scset.add(tarmaskposi[masksig]);
-						}
-						else
-						posi++;
-					}	
-				}
-			}
-			
-			if (m_horizontal)
-			posi += scset.size();
-			
-			return posi;
-			
-		}
-		
-		public double calc_score_px(int red1, int green1, int blue1, int red2, int green2, int blue2) {
-			int RG1=0; int BG1=0; int GR1=0; int GB1=0; int RB1=0; int BR1=0;
-			int RG2=0; int BG2=0; int GR2=0; int GB2=0; int RB2=0; int BR2=0;
-			double rb1=0; double rg1=0; double gb1=0; double gr1=0; double br1=0; double bg1=0;
-			double rb2=0; double rg2=0; double gb2=0; double gr2=0; double br2=0; double bg2=0;
-			double pxGap=10000; 
-			double BrBg=0.354862745; double BgGb=0.996078431; double GbGr=0.505882353; double GrRg=0.996078431; double RgRb=0.505882353;
-			double BrGap=0; double BgGap=0; double GbGap=0; double GrGap=0; double RgGap=0; double RbGap=0;
-			
-			if(blue1>red1 && blue1>green1){//1,2
-				if(red1>green1){
-					BR1=blue1+red1;//1
-					if(blue1!=0 && red1!=0)
-					br1= (double) red1 / (double) blue1;
-				}else{
-					BG1=blue1+green1;//2
-					if(blue1!=0 && green1!=0)
-					bg1= (double) green1 / (double) blue1;
-				}
-			}else if(green1>blue1 && green1>red1){//3,4
-				if(blue1>red1){
-					GB1=green1+blue1;//3
-					if(green1!=0 && blue1!=0)
-					gb1= (double) blue1 / (double) green1;
-				}else{
-					GR1=green1+red1;//4
-					if(green1!=0 && red1!=0)
-					gr1= (double) red1 / (double) green1;
-				}
-			}else if(red1>blue1 && red1>green1){//5,6
-				if(green1>blue1){
-					RG1=red1+green1;//5
-					if(red1!=0 && green1!=0)
-					rg1= (double) green1 / (double) red1;
-				}else{
-					RB1=red1+blue1;//6
-					if(red1!=0 && blue1!=0)
-					rb1= (double) blue1 / (double) red1;
-				}
-			}
-			
-			if(blue2>red2 && blue2>green2){
-				if(red2>green2){//1, data
-					BR2=blue2+red2;
-					if(blue2!=0 && red2!=0)
-					br2= (double) red2 / (double) blue2;
-				}else{//2, data
-					BG2=blue2+green2;
-					if(blue2!=0 && green2!=0)
-					bg2= (double) green2 / (double) blue2;
-				}
-			}else if(green2>blue2 && green2>red2){
-				if(blue2>red2){//3, data
-					GB2=green2+blue2;
-					if(green2!=0 && blue2!=0)
-					gb2= (double) blue2 / (double) green2;
-				}else{//4, data
-					GR2=green2+red2;
-					if(green2!=0 && red2!=0)
-					gr2= (double) red2 / (double) green2;
-				}
-			}else if(red2>blue2 && red2>green2){
-				if(green2>blue2){//5, data
-					RG2=red2+green2;
-					if(red2!=0 && green2!=0)
-					rg2= (double) green2 / (double) red2;
-				}else{//6, data
-					RB2=red2+blue2;
-					if(red2!=0 && blue2!=0)
-					rb2= (double) blue2 / (double) red2;
-				}
-			}
-			
-			///////////////////////////////////////////////////////					
-			if(BR1>0){//1, mask// 2 color advance core
-				if(BR2>0){//1, data
-					if(br1>0 && br2>0){
-						if(br1!=br2){
-							pxGap=br2-br1;
-							pxGap=Math.abs(pxGap);
-						}else
-						pxGap=0;
-						
-						if(br1==255 & br2==255)
-						pxGap=1000;
-					}
-				}else if (BG2>0){//2, data
-					if(br1<0.44 && bg2<0.54){
-						BrGap=br1-BrBg;//BrBg=0.354862745;
-						BgGap=bg2-BrBg;//BrBg=0.354862745;
-						pxGap=BrGap+BgGap;
-					}
-				}
-				//		IJ.log("pxGap; "+String.valueOf(pxGap)+"  BR1;"+String.valueOf(BR1)+", br1; "+String.valueOf(br1)+", BR2; "+String.valueOf(BR2)+", br2; "+String.valueOf(br2)+", BG2; "+String.valueOf(BG2)+", bg2; "+String.valueOf(bg2));
-			}else if(BG1>0){//2, mask/////////////////////////////
-				if(BG2>0){//2, data, 2,mask
-					
-					if(bg1>0 && bg2>0){
-						if(bg1!=bg2){
-							pxGap=bg2-bg1;
-							pxGap=Math.abs(pxGap);
-							
-						}else if(bg1==bg2)
-						pxGap=0;
-						if(bg1==255 & bg2==255)
-						pxGap=1000;
-					}
-					//	IJ.log(" pxGap BG2;"+String.valueOf(pxGap)+", bg1; "+String.valueOf(bg1)+", bg2; "+String.valueOf(bg2));
-				}else if(GB2>0){//3, data, 2,mask
-					if(bg1>0.8 && gb2>0.8){
-						BgGap=BgGb-bg1;//BgGb=0.996078431;
-						GbGap=BgGb-gb2;//BgGb=0.996078431;
-						pxGap=BgGap+GbGap;
-						//			IJ.log(" pxGap GB2;"+String.valueOf(pxGap));
-					}
-				}else if(BR2>0){//1, data, 2,mask
-					if(bg1<0.54 && br2<0.44){
-						BgGap=bg1-BrBg;//BrBg=0.354862745;
-						BrGap=br2-BrBg;//BrBg=0.354862745;
-						pxGap=BrGap+BgGap;
-					}
-				}
-				//		IJ.log("pxGap; "+String.valueOf(pxGap)+"  BG1;"+String.valueOf(BG1)+"  BG2;"+String.valueOf(BG2)+", bg1; "+String.valueOf(bg1)+", bg2; "+String.valueOf(bg2)+", GB2; "+String.valueOf(GB2)+", gb2; "+String.valueOf(gb2)+", BR2; "+String.valueOf(BR2)+", br2; "+String.valueOf(br2));
-			}else if(GB1>0){//3, mask/////////////////////////////
-				if(GB2>0){//3, data, 3mask
-					if(gb1>0 && gb2>0){
-						if(gb1!=gb2){
-							pxGap=gb2-gb1;
-							pxGap=Math.abs(pxGap);
-							
-							//	IJ.log(" pxGap GB2;"+String.valueOf(pxGap));
-						}else
-						pxGap=0;
-						if(gb1==255 & gb2==255)
-						pxGap=1000;
-					}
-				}else if(BG2>0){//2, data, 3mask
-					if(gb1>0.8 && bg2>0.8){
-						BgGap=BgGb-gb1;//BgGb=0.996078431;
-						GbGap=BgGb-bg2;//BgGb=0.996078431;
-						pxGap=BgGap+GbGap;
-					}
-				}else if(GR2>0){//4, data, 3mask
-					if(gb1<0.7 && gr2<0.7){
-						GbGap=gb1-GbGr;//GbGr=0.505882353;
-						GrGap=gr2-GbGr;//GbGr=0.505882353;
-						pxGap=GbGap+GrGap;
-					}
-				}//2,3,4 data, 3mask
-			}else if(GR1>0){//4mask/////////////////////////////
-				if(GR2>0){//4, data, 4mask
-					if(gr1>0 && gr2>0){
-						if(gr1!=gr2){
-							pxGap=gr2-gr1;
-							pxGap=Math.abs(pxGap);
-						}else
-						pxGap=0;
-						if(gr1==255 & gr2==255)
-						pxGap=1000;
-					}
-				}else if(GB2>0){//3, data, 4mask
-					if(gr1<0.7 && gb2<0.7){
-						GrGap=gr1-GbGr;//GbGr=0.505882353;
-						GbGap=gb2-GbGr;//GbGr=0.505882353;
-						pxGap=GrGap+GbGap;
-					}
-				}else if(RG2>0){//5, data, 4mask
-					if(gr1>0.8 && rg2>0.8){
-						GrGap=GrRg-gr1;//GrRg=0.996078431;
-						RgGap=GrRg-rg2;
-						pxGap=GrGap+RgGap;
-					}
-				}//3,4,5 data
-			}else if(RG1>0){//5, mask/////////////////////////////
-				if(RG2>0){//5, data, 5mask
-					if(rg1>0 && rg2>0){
-						if(rg1!=rg2){
-							pxGap=rg2-rg1;
-							pxGap=Math.abs(pxGap);
-						}else
-						pxGap=0;
-						if(rg1==255 & rg2==255)
-						pxGap=1000;
-					}
-					
-				}else if(GR2>0){//4 data, 5mask
-					if(rg1>0.8 && gr2>0.8){
-						GrGap=GrRg-gr2;//GrRg=0.996078431;
-						RgGap=GrRg-rg1;//GrRg=0.996078431;
-						pxGap=GrGap+RgGap;
-						//	IJ.log(" pxGap GR2;"+String.valueOf(pxGap));
-					}
-				}else if(RB2>0){//6 data, 5mask
-					if(rg1<0.7 && rb2<0.7){
-						RgGap=rg1-RgRb;//RgRb=0.505882353;
-						RbGap=rb2-RgRb;//RgRb=0.505882353;
-						pxGap=RbGap+RgGap;
-					}
-				}//4,5,6 data
-			}else if(RB1>0){//6, mask/////////////////////////////
-				if(RB2>0){//6, data, 6mask
-					if(rb1>0 && rb2>0){
-						if(rb1!=rb2){
-							pxGap=rb2-rb1;
-							pxGap=Math.abs(pxGap);
-						}else if(rb1==rb2)
-						pxGap=0;
-						if(rb1==255 & rb2==255)
-						pxGap=1000;
-					}
-				}else if(RG2>0){//5, data, 6mask
-					if(rg2<0.7 && rb1<0.7){
-						RgGap=rg2-RgRb;//RgRb=0.505882353;
-						RbGap=rb1-RgRb;//RgRb=0.505882353;
-						pxGap=RgGap+RbGap;
-						//	IJ.log(" pxGap RG;"+String.valueOf(pxGap));
-					}
-				}
-			}//2 color advance core
-			
-			return pxGap;
-		}
-=======
->>>>>>> parent of f1d5276 (horizontal image)
-=======
->>>>>>> parent of f1d5276 (horizontal image)
-=======
->>>>>>> parent of f1d5276 (horizontal image)
 		
 	}//public class ColorMIPMaskCompare {
 	
 } //public class Two_windows_mask_search implements PlugInFilter{
-
-
